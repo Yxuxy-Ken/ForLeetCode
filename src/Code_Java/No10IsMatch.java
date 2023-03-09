af9298aa6abb81659b30fe48150722e2cebe985d
chenkun <17715266531@163.com>
2023-03-09 10:27:11 +0800

    变更规范

---
+++ src/Code_Java/No10IsMatch.java
@@ -0,0 +1,24 @@
+package Code_Java;
+
+/**
+ * @author chenkun
+ * @since 2023年3月9日
+ *
+ *  * 给你一个字符串 s 和一个字符规律 p，请你来实现一个支持 '.' 和 '*' 的正则表达式匹配。
+ *  *
+ *  * '.' 匹配任意单个字符
+ *  * '*' 匹配零个或多个前面的那一个元素
+ *  * 所谓匹配，是要涵盖 整个 字符串 s的，而不是部分字符串。
+ */
+public class No10IsMatch {
+
+    public boolean isMatch(String s, String p) {
+        boolean result = false;
+
+        char[] str = s.toCharArray();
+
+        System.out.println(str[1]);
+
+        return result;
+
+}
--- src/DesignPatterns/LazySingleton.java
+++ src/DesignPatterns/LazySingleton.java
@@ -3,11 +3,15 @@ package DesignPatterns;
 
 public class LazySingleton {
 
-    //保证 instance 在所有线程中同步
+    /**
+     * 保证 instance 在所有线程中同步
+     */
     private static volatile LazySingleton instance = null;
     //volatile 修改的值会立刻更新到RAM中,确保可见性
 
-    //private 构造器私有,避免类在外部被实例化
+    /**
+     * private 构造器私有,避免类在外部被实例化
+     */
     private LazySingleton() {
     }
 
